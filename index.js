cf973ac964926a5e3ff31b56c1ab8a0a5afa6002
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-14 15:30:20 +0700

    Create basic listener functions

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('HELLO WORLD')
+;(function() {
+  // ---------------------------------------------------------------------------
+
+  const registerForm = $('#register-form')
+  const registerEmail = $('#register-email')
+  const registerPassword = $('#register-password')
+
+  const loginForm = $('#login-form')
+  const loginEmail = $('#login-email')
+  const loginPassword = $('#login-password')
+
+  // ---------------------------------------------------------------------------
+
+  const register = () => {
+    const email = registerEmail.val()
+    const password = registerPassword.val()
+
+    console.log('register', email, password)
+  }
+
+  const login = () => {
+    const email = loginEmail.val()
+    const password = loginPassword.val()
+
+    console.log('login:', email, password)
+  }
+
+  // ---------------------------------------------------------------------------
+
+  registerForm.on('submit', event => {
+    event.preventDefault()
+
+    register()
+  })
+
+  loginForm.on('submit', event => {
+    event.preventDefault()
+
+    login()
+  })
+})()
